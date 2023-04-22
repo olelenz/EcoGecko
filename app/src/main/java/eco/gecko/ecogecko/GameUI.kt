750c9c6abc0486f41d2e4b13b82276b4645f80c2
daschdy <dustin.heither@gmail.com>
2023-04-22 19:46:40 +0200

    better grid

--- app/src/main/java/eco/gecko/ecogecko/GameUI.kt
+++ app/src/main/java/eco/gecko/ecogecko/GameUI.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.GridView
 import androidx.fragment.app.Fragment
 
+
 class GameUI : Fragment() {
 
     private lateinit var grid: GridView
@@ -35,40 +36,33 @@ class GameUI : Fragment() {
     }
 
     private fun createNewGame() {
-        val dataSource = listOf(1, 0, 1, 1, 0, 0,
-                                0, 1, 0, 1, 1, 0,
-                                1, 0, 1, 1, 0, 0,
-                                0, 1, 0, 1, 1, 0,
-                                1, 0, 1, 1, 0, 0,
-                                0, 1, 0, 1, 1, 0)
-        val adapter = GridAdapter(dataSource, n*n)
+        val adapter = GridAdapter()
         grid.adapter = adapter
     }
 }
 
-/**
- * GRID CLass with basic functions
- */
+class GridAdapter() : BaseAdapter() {
 
-class GridAdapter(private val dataSource: List<Int>, private val gridSize: Int) : BaseAdapter() {
-    private val rowCount = kotlin.math.ceil(dataSource.size.toDouble() / gridSize).toInt()
-    private val gridDataSource = dataSource + List((rowCount * gridSize) - dataSource.size) { 0 }
+    val dataSource = listOf(1, 0, 1, 1, 0, 0,
+                            0, 1, 0, 1, 1, 0,
+                            1, 0, 1, 1, 0, 0,
+                            0, 1, 0, 1, 1, 0,
+                            1, 0, 1, 1, 0, 0,
+                            0, 1, 0, 1, 1, 0)
+    override fun getCount(): Int = dataSource.size
 
-    override fun getCount(): Int = gridDataSource.size
-
-    override fun getItem(position: Int): Any = gridDataSource[position]
+    override fun getItem(position: Int): Any = dataSource[position]
 
     override fun getItemId(position: Int): Long = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = convertView ?: LayoutInflater.from(parent?.context).inflate(R.layout.tile, parent, false)
+        val view = convertView ?: LayoutInflater.from(parent?.context).inflate(R.layout.list_layout, parent, false)
 
-        // set background color based on item value
-        when (gridDataSource[position]) {
-            0 -> view.setBackgroundColor(Color.BLUE)
-            1 -> view.setBackgroundColor(Color.RED)
-        }
+        view.setBackgroundResource(R.drawable.tiles_layout)
+        view.setPadding(12,0,12,0)
 
         return view
     }
 }
+
+
