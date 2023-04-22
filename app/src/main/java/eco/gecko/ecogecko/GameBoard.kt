de025e4b1c4b5d0d4559d930c0953f8e9bb8f18a
Ole <ol02@gmx.net>
2023-04-22 13:15:25 +0200

    move tile up, down, left, right and check if allowed

--- app/src/main/java/eco/gecko/ecogecko/GameBoard.kt
+++ app/src/main/java/eco/gecko/ecogecko/GameBoard.kt
@@ -2,4 +2,33 @@ package eco.gecko.ecogecko
 
 class GameBoard {
 
+    companion object{
+        private const val BOARD_SIZE_X: Int = 6
+        private const val BOARD_SIZE_Y: Int = 6
+
+        /*
+        Function to check if a set of coordinates are inside the board
+            */
+        fun checkCoordinates(xPositions: Array<Int>, yPositions: Array<Int>): Boolean{
+            if (xPositions.size != yPositions.size){
+                throw IllegalStateException("Position arrays do not have the same size!")
+            }
+            var ind: Int = 0
+            var out: Boolean = true
+            while (ind < xPositions.size){
+                out = out && checkCoordinateValid(xPositions[ind],yPositions[ind])
+                ind++
+            }
+            return out
+        }
+
+        /*
+        Function to check single coordinate and check if it is inside the board
+            */
+        fun checkCoordinateValid(x: Int, y: Int): Boolean{
+            return !(x < 0 || y < 0 || x >= BOARD_SIZE_X || y >= BOARD_SIZE_Y)
+        }
+    }
+
+
 }
--- app/src/main/java/eco/gecko/ecogecko/Tile.kt
+++ app/src/main/java/eco/gecko/ecogecko/Tile.kt
@@ -1,5 +1,67 @@
 package eco.gecko.ecogecko
 
-class Tile {
+import eco.gecko.ecogecko.GameBoard.Companion.checkCoordinateValid
+import eco.gecko.ecogecko.GameBoard.Companion.checkCoordinates
+
+class Tile(private var positionsX: Array<Int>, private var positionsY: Array<Int>) {
+    public fun getPositionX():Array<Int>{
+        return positionsX
+    }
+
+    public fun getPositionY():Array<Int>{
+        return positionsY
+    }
+
+    public fun moveUp() {
+        var newPositionsY = positionsY.clone()
+        for (ind in 0..positionsY.size){
+            newPositionsY[0] = newPositionsY[0] + 1
+        }
+        if (checkCoordinates(positionsX, newPositionsY)){
+            positionsY = newPositionsY.clone()
+        } else {
+            throw IndexOutOfBoundsException("You can not move here!")
+        }
+    }
+
+    public fun moveDown() {
+        var newPositionsY = positionsY.clone()
+        for (ind in 0..positionsY.size){
+            newPositionsY[0] = newPositionsY[0] - 1
+        }
+        if (checkCoordinates(positionsX, newPositionsY)){
+            positionsY = newPositionsY.clone()
+        } else {
+            throw IndexOutOfBoundsException("You can not move here!")
+        }
+    }
+
+    public fun moveLeft() {
+        var newPositionsX = positionsX.clone()
+        for (ind in 0..positionsX.size){
+            newPositionsX[0] = newPositionsX[0] - 1
+        }
+        if (checkCoordinates(newPositionsX, positionsY)){
+            positionsX = newPositionsX.clone()
+        } else {
+            throw IndexOutOfBoundsException("You can not move here!")
+        }
+    }
+
+    public fun moveRight() {
+        var newPositionsX = positionsX.clone()
+        for (ind in 0..positionsX.size){
+            newPositionsX[0] = newPositionsX[0] + 1
+        }
+        if (checkCoordinates(newPositionsX, positionsY)){
+            positionsX = newPositionsX.clone()
+        } else {
+            throw IndexOutOfBoundsException("You can not move here!")
+        }
+    }
+
+
+
+
 
 }
